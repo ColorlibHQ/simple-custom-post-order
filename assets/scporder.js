814e2613d0599ba76f45e0ce18bc9adaa3f9bca4
George Ciobanu <george.ciobanu91@gmail.com>
2020-04-28 14:13:00 +0300

    https://github.com/ColorlibHQ/simple-custom-post-order/issues/71

--- assets/scporder.js
+++ assets/scporder.js
@@ -32,7 +32,6 @@
      * Fix for table breaking
      */
     jQuery(window).load(function () {
-        jQuery('#the-list').width(jQuery('#the-list').width());
 
         // make the array for the sizes
         var td_array = new Array();
@@ -46,7 +45,6 @@
         jQuery('#the-list').find('tr').each(function () {
             var j = 0;
             $(this).find('td').each(function () {
-                $(this).width(td_array[j]);
                 $(this).css('padding','8px 0px');
                 j += 1;
             });
@@ -57,13 +55,11 @@
         // check if there are no items in the table
         if(jQuery('#the-list > tr.no-items').length == 0){
             jQuery('#the-list').parent().find('thead').find('th').each(function () {
-                $(this).width(td_array[y]);
                 $(this).css('padding','8px 0px');
                 y += 1;
             });
 
             jQuery('#the-list').parent().find('tfoot').find('th').each(function () {
-                $(this).width(td_array[y]);
                 $(this).css('padding','8px 0px');
                 y += 1;
             });
